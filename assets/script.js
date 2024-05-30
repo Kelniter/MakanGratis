56806b60ae5396fe316f31bb08285d11cbfc87f7
Kelpin <158505065+Kelniter@users.noreply.github.com>
2024-05-30 17:12:06 +0000

    Hi ! Magic

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 const parter = ["GoogleOnPoint", "MachineOnPoint"]
 
+// window.onload = OnProfile()
+
 function OnAll() {
   for (let index = 0; index < parter.length; index++){
     const pointer = parter[index];
@@ -32,3 +34,22 @@ function ProjectOn(pinpoint) {
   }
   HideOn(pinpoint);
 }
+
+function OnProfile() {
+  const profiler = document.getElementById("urlprofile");
+  const urla = profiler.querySelectorAll("a");
+  const icon = profiler.querySelectorAll("i");
+  fetch("../notation/prourl.json").then(
+    response => response.json()).then(
+      data => {
+        for (let i = 0; i < urla.length; i++) {
+          const prolink = urla[i]
+          const proicon = icon[i]
+          prolink.href = data[Object.keys(data)[i]].url;
+          proicon.className = data[Object.keys(data)[i]].class;
+        }
+      }).catch(error => {
+        console.error(error);
+      })
+}
+// window.onload = Keltion();
